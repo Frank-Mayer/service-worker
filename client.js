4736ad68f021575e6078e881dc84fb2f1c8d7713
Frank-Mayer <mail@frank-mayer.io>
2022-09-13 17:05:59 +0200

    client code

--- client.js
+++ client.js
@@ -2,8 +2,9 @@ export const CHANNEL = new BroadcastChannel("@frank-mayer/service-worker");
 
 /**
  * Get notified when the service workers offline first cache has been updated.
+ * @param {() => void} callback - The callback to be called when the cache has been updated.
  */
-export const onServiceWorkerCacheUpdated = (callback: () => void) => {
+export const onServiceWorkerCacheUpdated = (callback) => {
   CHANNEL.addEventListener("message", (ev) => {
     if (
       ev.data.from === "@frank-mayer/service-worker" &&
